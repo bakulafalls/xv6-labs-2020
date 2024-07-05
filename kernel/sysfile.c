60f6c6310c826e472ec939080fe31f34773d59d5
root <450969657@qq.com>
2024-07-05 21:07:34 +0800

    finished task2, TODO: make grade pass

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -16,6 +16,8 @@
 #include "file.h"
 #include "fcntl.h"
 
+#define MAX_SYMLINK_DEPTH 10
+
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
 static int
@@ -322,6 +324,37 @@ sys_open(void)
     return -1;
   }
 
+  // Symbolic link
+  if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {   // still point to a symbol link
+    // recrusion until we find a real file
+    for(int i = 0; i < MAX_SYMLINK_DEPTH; i++) {
+      // ip = where the symbol link point to
+      if(readi(ip, 0, (uint64)path, 0, MAXPATH) != MAXPATH) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      iunlockput(ip);
+      ip = namei(path);
+
+      if(ip == 0) {
+        end_op();
+        return -1;
+      }
+
+      ilock(ip);
+      if(ip->type != T_SYMLINK)  // found the real file
+        break;
+    }
+
+    // Exceed the MAX_SYMLINK_PATH
+    if(ip->type == T_SYMLINK) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+  }
+
   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
     if(f)
       fileclose(f);
@@ -482,5 +515,37 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0) {
+    return -1;
+  }
+
+  begin_op();
+  
+  // alloc an inode 
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0) {  // alloc inode failed
+    end_op();
+    return -1;
+  }
+
+  // write target into ip
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) != MAXPATH) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
+
   return 0;
 }
